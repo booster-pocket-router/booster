9590e1a1679065412b783f494a2725cffadba0f1
Daniel Morandini <danielmorandini@me.com>
2018-12-12 11:07:07 +0100

    Add copyright header

--- source/source.go
+++ source/source.go
@@ -1,3 +1,19 @@
+/* Copyright (C) 2018 KIM KeepInMind GmbH/srl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package source
 
 import (
@@ -5,6 +21,7 @@ import (
 )
 
 func WithValue(parent core.Source, key interface{}, value interface{}) core.Source {
+	// TODO: implement
 	return parent
 }
 
