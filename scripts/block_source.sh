bb193d4b6ce06086e3e652abbb7b05de9a1df3a9
Daniel Morandini <danielmorandini@me.com>
2018-12-13 12:19:03 +0100

    Request to set HTTP method to use (so we can block/free policies with the same script)

--- scripts/block_source.sh
+++ scripts/block_source.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-ifi="$1"
-if [ -z "$ifi" ]; then
+METHOD="$1"
+NAME="$2"
+
+if [ -z "$METHOD" ]; then
+	echo "You have to pass the name of the method that you want to execute for this to work out:"
+	echo "Only POST or DELETE will produce valid results."
+	echo "POST: creates the block policy"
+	echo "DELETE: removes a previously created block policy"
+	echo "Example: $0 POST en0"
+	exit 0
+fi
+if [ -z "$NAME" ]; then
 	echo "You have to pass the name of the interface that you want to block for this to work out:"
-	echo "Example: $0 en0"
+	echo "Example: $0 POST en0"
 	exit 0
 fi
 
-curl -H "Content-Type: application/json" -d "{\"reason\": \"blocked by $0 script\"}" -X POST http://localhost:7764/sources/"$ifi"/block
+curl -H "Content-Type: application/json" -d "{\"reason\": \"blocked by $0 script\"}" -X "$METHOD" http://localhost:7764/sources/"$NAME"/block
