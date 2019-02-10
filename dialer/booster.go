222b61f0360d1c9a3257a18d361dfcc0b0fb08bb
Daniel Morandini <danielmorandini@me.com>
2019-02-10 14:27:09 +0100

    Add dialer's package doc

--- dialer/booster.go
+++ dialer/booster.go
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+// Package dialer provides a connection dialer that opens new
+// `net.Conn`s using the sources provided by its Balancer.
 package dialer
 
 import (
